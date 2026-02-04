13a56fbc43b8af043823ab53ec21d92e0827c828
Kody Spring <36212983+kodyspring@users.noreply.github.com>
2026-02-03 22:53:31 -0500

    clean up main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
-
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
